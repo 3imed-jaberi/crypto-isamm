e58b2ae6d725cce2fb0043a027a607932bc83a35
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-20 23:54:54 +0100

    main container 🔥 ..

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import AppNavigator from './src';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+
+const App = () => {
+
+  const [isReady, setIsReady] = useState(false);
+
+
+  const handleFinshLoading = () => setIsReady(true);
+
+
+  const loadAssets = async () => {
+
+    const cacheImages = (images) => images.map(image => (typeof image === 'string') ? Image.prefetch(image) : Asset.fromModule(image).downloadAsync());
+
+    const cacheFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
+
+
+    const imageAssets = cacheImages([
+      require('./src/assets/img/logo.png')
+    ]);
+
+    const fontAssets = cacheFonts([{
+      'ample-soft-pro': require('./src/assets/fonts/Ample-Soft-Pro.otf')
+    }]);
+
+
+    await Promise.all([...fontAssets, ...imageAssets]);
+  }
+
+  return(
+    !isReady 
+        ? 
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={handleFinshLoading}
+      onError={console.warn}
+    />
+        :
+    <AppNavigator />
+  );    
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default App ;
